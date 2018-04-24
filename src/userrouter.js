f9f8f353699d85ef32df4d979ddbb2e74e360346
Jens Olsson <jenson@axis.com>
2018-04-24 19:08:25 +0200

    Add rate limit of emails

--- src/userrouter.js
+++ src/userrouter.js
@@ -5,13 +5,46 @@ const config = require('./config')
 
 const server = email.server.connect(config.get('email'))
 
-module.exports = db => {
+module.exports = () => {
   const user = express.Router()
+
+  /*
+   * Use blacklists to prevent spamming.
+   * If an item is present in the blacklist
+   * the request will not be processed.
+   * Clearing out the ip blacklist often
+   * allows for NAT addresses, but there is
+   * no need to send the same email more
+   * than once per day.
+   */
+  const blacklists = {
+    ip: {},
+    email: {}
+  }
+  setInterval(() => {
+    blacklists.ip = {}
+  }, 60 * 1000)
+
+  setInterval(() => {
+    blacklists.email = {}
+  }, 24* 60 * 60 * 1000)
+
   user.post('/sendToken', (req, res) => {
     const email = req.body.email
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+
     if (!email) {
       return res.status(400).end()
     }
+
+    if (blacklists.ip[ip] || blacklists.email[email]) {
+      console.log(`Did not send email: ${blacklists.ip[ip] || blacklists.email[email]}`)
+      return res.status(429).end()
+    }
+
+    blacklists.ip[ip] = ip
+    blacklists.email[email] = email
+
     const token = auth.createToken(email)
     const htmlmessage = `<html>
       i <i>hope</i> this works!
