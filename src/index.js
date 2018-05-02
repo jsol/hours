aa642989bea2333b0324d8b2ad89449fc9bf88d4
Jens Olsson <jenson@axis.com>
2018-05-02 16:40:11 +0200

    Fix escape-fails on iOs devices
    Seems like the # gets escaped in the URLs sometimes.
    No idea why.
    Fixing the symptom instead of the cause by redirecting
    urls that matches the pattern to their un-escaped counterpart.

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ const app = express()
 
 const dbPool = mysql.createPool(config.get('mysql'))
 
+const hashEscaped = new RegExp('/(mobile|desktop)_init.html%23(.*)')
+
 function nocache (req, res, next) {
   res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
   res.header('Expires', '-1')
@@ -28,5 +30,16 @@ app.use('/v1/holiday', auth.auth, holidayRouter(dbPool))
 app.use('/v1/user', userRouter(dbPool))
 
 app.use('/', express.static(path.join(__dirname, '../www')))
+
+app.use((req, res, next) => {
+  // Apparantly the # in the url can somehow get escaped in iOs devices,
+  // if we receive an URL with an obvious escape-fail, redirect it to the
+  // proper url.
+  const parts = req.url.match(hashEscaped)
+  if (parts) {
+    return res.redirect(`/${parts[1]}_init.html#${parts[2]}`)
+  }
+  res.status(404).end('<html><body>The URL is not valid. Please go to the <a href="/">start page</a> instead.</body></html>')
+})
 
 app.listen(config.get('app.port'))
